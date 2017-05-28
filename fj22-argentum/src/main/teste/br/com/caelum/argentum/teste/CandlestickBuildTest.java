ed6de2654521bb643bcb4a7af137490991c2d301
Luiz Henrique <luizhenrique.ecl@gmail.com>
2017-05-28 16:11:38 -0300

    Fazendo exercício de testes da aplicacao

---
+++ fj22-argentum/src/main/teste/br/com/caelum/argentum/teste/CandlestickBuildTest.java
@@ -0,0 +1,11 @@
+package br.com.caelum.argentum.teste;
+
+import org.junit.Test;
+
+public class CandlestickBuildTest {
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void geracao_de_candle_deve_ter_todos_os_dados_necessarios() {
+		//TODO: crirar Teste
+	}
+}
